c8f437886555a3fefe2272aeb11a68050e72a11b
mhdtaufikrm <mtaufikramadhan27@gmail.com>
2023-07-06 14:43:06 +0700

    done deploy dropdown and user

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\DropdownController;
 use App\Http\Controllers\RulesController;
+use App\Http\Controllers\UserController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -21,9 +23,19 @@ Route::get('/home', [HomeController::class, 'index']);
 
 //Dropdown Controller
 Route::get('/dropdown', [DropdownController::class, 'index']);
+Route::post('/dropdown/store', [DropdownController::class, 'store']);
+Route::patch('/dropdown/update/{id}', [DropdownController::class, 'update']);
+Route::delete('/dropdown/delete/{id}', [DropdownController::class, 'delete']);
 
 //Rules Controller
 Route::get('/rule', [RulesController::class, 'index']);
 Route::post('/rule/store', [RulesController::class, 'store']);
 Route::patch('/rule/update/{id}', [RulesController::class, 'update']);
 Route::delete('/rule/delete/{id}', [RulesController::class, 'delete']);
+
+//User Controller
+Route::get('/user', [UserController::class, 'index']);
+Route::post('/user/store', [UserController::class, 'store']);
+Route::patch('/user/update/{user}',[UserController::class, 'update']);
+Route::get('/user/revoke/{user}',[UserController::class, 'revoke']);
+Route::get('/user/access/{user}',[UserController::class, 'access']);
