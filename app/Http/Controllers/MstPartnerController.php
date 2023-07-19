573f640074a16eba3628984452056205d6a32e17
mhdtaufikrm <mtaufikramadhan27@gmail.com>
2023-07-19 10:54:32 +0700

    fix dropdown province

--- app/Http/Controllers/MstPartnerController.php
+++ app/Http/Controllers/MstPartnerController.php
@@ -16,8 +16,34 @@ class MstPartnerController extends Controller
     {
         $partner = MstPartner::select('*')
         ->get();
+        //API Regional
+        $ruleAuthRegional = Rule::where('rule_name', 'API Auth Regional')->first();
+        $url_AuthRegional = $ruleAuthRegional->rule_value;
 
-        return view('partner.index',compact('partner'));
+        $ruleEmailRegional = Rule::where('rule_name', 'Email Auth Regional')->first();
+        $emailRegional = $ruleEmailRegional->rule_value;
+
+        $rulePasswordRegional = Rule::where('rule_name', 'Password Auth Regional')->first();
+        $passwordRegional = $rulePasswordRegional->rule_value;
+
+        $response = Http::post($url_AuthRegional, [
+            'email' => $emailRegional,
+            'password' => $passwordRegional,
+        ]);
+
+        $data = $response['data'];
+        $token = $data['token'];
+
+        //get list province
+        $ruleApiProvince = Rule::where('rule_name', 'API List Province')->first();
+        $url_ApiProvince = $ruleApiProvince->rule_value;
+
+        $getProvince = Http::withToken($token)
+            ->get($url_ApiProvince);
+        $provinces = $getProvince['data'];
+        //End API Regional
+
+        return view('partner.index',compact('partner','provinces'));
     }
 
     public function storePartner(Request $request)
--- resources/views/partner/index.blade.php
+++ resources/views/partner/index.blade.php
@@ -9,7 +9,7 @@
                     <div class="col-auto mt-4">
                         <h1 class="page-header-title">
                             <div class="page-header-icon"><i data-feather="tool"></i></div>
-                            Dropdown App Menu
+                            Rule App Menu
                         </h1>
                         <div class="page-header-subtitle">Use this blank page as a starting point for creating new pages inside your project!</div>
                     </div>
@@ -40,13 +40,13 @@
           <div class="col-12">
             <div class="card">
               <div class="card-header">
-                <h3 class="card-title">List of Partner</h3>
+                <h3 class="card-title">List of Rule</h3>
               </div>
               
               <!-- /.card-header -->
               <div class="card-body">
                 <div class="row">
-                    <div class="mb-3 col">
+                    <div class="mb-3 col-sm-6">
                         <button type="button" class="btn btn-primary btn-sm mb-2" data-bs-toggle="modal" data-bs-target="#modal-add">
                             <i class="fas fa-plus-square"></i> 
                           </button>
@@ -226,18 +226,27 @@
                           </div>
                           
 
-                    <div class="col">
+                    <div class="col-sm-6">
                       <!--alert success -->
                       @if (session('status'))
-                        <div class="alert alert-success alert-dismissible fade show" role="alert">
-                          <strong>{{ session('status') }}</strong>
-                        </div> 
-                      @endif
+                      <div class="alert alert-success alert-dismissible fade show" role="alert">
+                        <strong>{{ session('status') }}</strong>
+                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                      </div> 
+                    @endif
+
+                    @if (session('failed'))
+                    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                      <strong>{{ session('failed') }}</strong>
+                      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                    </div> 
+                  @endif
                     
                       <!--alert success -->
                       <!--validasi form-->
                         @if (count($errors)>0)
                           <div class="alert alert-info alert-dismissible fade show" role="alert">
+                            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
                               <ul>
                                   <li><strong>Data Process Failed !</strong></li>
                                   @foreach ($errors->all() as $error)
@@ -250,14 +259,12 @@
                     </div>
                 </div>
                 <div class="table-responsive"> 
-                <table id="tablePartner" class="table table-bordered table-striped">
+                <table id="tableUser" class="table table-bordered table-striped">
                   <thead>
                   <tr>
                     <th>No</th>
-                    <th>Name</th>
-                    <th>Email</th>
-                    <th>Role</th>
-                    <th>Last Login</th>
+                    <th>Rule Name</th>
+                    <th>Rule Value</th>
                     <th>Action</th>
                   </tr>
                   </thead>
@@ -265,39 +272,109 @@
                     @php
                       $no=1;
                     @endphp
-                    {{-- @foreach ($user as $data) --}}
+                    @foreach ($partner as $data)
                     <tr>
-                        <td></td>
+                        <td>{{ $no++ }}</td>
+                        <td>{{ $data->rule_name }}</td>
+                        <td>{{ $data->rule_value }}</td>
+                        <td>
+                            <button title="Edit Rule" class="btn btn-primary btn-sm" data-bs-toggle="modal" data-bs-target="#modal-update{{ $data->id }}">
+                                <i class="fas fa-edit"></i>
+                              </button>
+                            <button title="Delete Rule" class="btn btn-danger btn-sm" data-bs-toggle="modal" data-bs-target="#modal-delete{{ $data->id }}">
+                                <i class="fas fa-trash-alt"></i>
+                              </button>   
+                        </td>
                     </tr>
-                    {{-- Modal Revoke --}}
-                    <div class="modal fade" id="modal-revoke">
+
+                    {{-- Modal Update --}}
+                    <div class="modal fade" id="modal-update{{ $data->id }}" tabindex="-1" aria-labelledby="modal-update{{ $data->id }}-label" aria-hidden="true">
                         <div class="modal-dialog">
-                            <div class="modal-content">
-                                <div class="modal-header">
-                                    <h4 class="modal-title">Revoke User Access</h4>
-                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                </div>
-                                <form action="" enctype="multipart/form-data" method="GET">
-                                    @csrf
-                                    <div class="modal-body">
-                                        <div class="form-group">
-                                            Are you sure you want to revoke <label for="email"></label>?
-                                        </div>
-                                    </div>
-                                    <div class="modal-footer justify-content-between">
-                                        <button type="button" class="btn btn-default" data-bs-dismiss="modal">Close</button>
-                                        <input type="submit" class="btn btn-primary" value="Submit">
-                                    </div>
-                                </form>
+                          <div class="modal-content">
+                            <div class="modal-header">
+                              <h4 class="modal-title" id="modal-update{{ $data->id }}-label">Edit Rule</h4>
+                              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                             </div>
+                            <form action="{{ url('/rule/update/'.$data->id) }}" method="POST">
+                              @csrf
+                              @method('patch')
+                              <div class="modal-body">
+                                <div class="form-group">
+                                  <input name="id" type="text" value="{{$data->id}}" hidden>
+                                  <input type="text" class="form-control" id="rule_name" name="rule_name" placeholder="Enter Rule Name" value="{{ $data->rule_name }}">
+                                </div>
+                                <br>
+                                <div class="form-group">
+                                  <input type="text" class="form-control" id="rule_value" name="rule_value" placeholder="Enter Rule Value" value="{{ $data->rule_value }}">
+                                </div>
+                              </div>
+                              <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                <button type="submit" class="btn btn-primary">Update</button>
+                              </div>
+                            </form>
+                          </div>
                         </div>
+                      </div>
+                    {{-- Modal Update --}}
+
+                    {{-- Modal Delete --}}
+                    <div class="modal fade" id="modal-delete{{ $data->id }}" tabindex="-1" aria-labelledby="modal-delete{{ $data->id }}-label" aria-hidden="true">
+                        <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                            <h4 class="modal-title" id="modal-delete{{ $data->id }}-label">Delete Rule</h4>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <form action="{{ url('/rule/delete/'.$data->id) }}" method="POST">
+                            @csrf
+                            @method('delete')
+                            <div class="modal-body">
+                                <div class="form-group">
+                                Are you sure you want to delete <label for="rule">{{ $data->rule_name }}</label>?
+                                </div>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                <button type="submit" class="btn btn-primary">Submit</button>
+                            </div>
+                            </form>
+                        </div>
+                        </div>
+                    </div>
+                    {{-- Modal Delete --}}
+
+                    {{-- Modal Access --}}
+                    <div class="modal fade" id="modal-access}">
+                      <div class="modal-dialog">
+                          <div class="modal-content">
+                          <div class="modal-header">
+                              <h4 class="modal-title">Give User Access</h4>
+                              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                              <span aria-hidden="true">&times;</span>
+                              </button>
+                          </div>
+                          <form action="{{ url('') }}" enctype="multipart/form-data" method="GET">
+                          @csrf
+                          <div class="modal-body">
+                            
+                          </div>
+                          <div class="modal-footer justify-content-between">
+                            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                            <input type="submit" class="btn btn-primary" value="Submit">
+                          </div>
+                          </form>
+                          </div>
+                          <!-- /.modal-content -->
+                      </div>
+                    <!-- /.modal-dialog -->
                     </div>
                     {{-- Modal Revoke --}}
 
-                    {{-- @endforeach --}}
+                    @endforeach
                   </tbody>
                 </table>
-            </div>
+              </div>
               </div>
               <!-- /.card-body -->
             </div>
@@ -319,7 +396,7 @@
 <!-- For Datatables -->
 <script>
     $(document).ready(function() {
-      var table = $("#tablePartner").DataTable({
+      var table = $("#tableUser").DataTable({
         "responsive": true, 
         "lengthChange": false, 
         "autoWidth": false,
@@ -327,7 +404,8 @@
       });
     });
   </script>
-  <script type="text/javascript">
+
+<script type="text/javascript">
     //ajax mapping city
         // CSRF Token
         var CSRF_TOKEN = $('meta[name="csrf-token"]').attr('content');
@@ -465,4 +543,5 @@
 
             });
 </script>
+
 @endsection
