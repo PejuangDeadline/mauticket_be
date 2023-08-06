96dde1bd603239e906874edfdfdfcc969c528d9c
julia eka <eka47577@gmail.com>
2023-08-06 22:18:20 +0700

    need 1 parameter for update delete master show time

--- app/Http/Controllers/ShowtimeController.php
+++ app/Http/Controllers/ShowtimeController.php
@@ -2,38 +2,113 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use App\Models\Showtime;
 use App\Models\Event;
+use App\Models\Showtime;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
+
 
 class ShowtimeController extends Controller
 {
-    public function index($id){
+    public function index($id)
+    {
         //need Query for eventName in scheme event->event_name
         $id = decrypt($id);
-        $event = Event::where('id',$id)
-        ->first();
-        $showTime = Showtime::where('id_event',$id)->get();
-        return view('show-time.index',compact('showTime','id','event'));
-    }
-    public function store(Request $request, $id){    
-        dd($request);
-        $id = decrypt($id);
-       $showTime = Showtime::where('id_event',$id)->get();
-        return view('show-time.index',compact('showTime'));
+        // dd('test');
+        $event = Event::where('id', $id)->first();
+        $showTime = Showtime::where('id_event', $id)->get();
+        return view('show-time.index', compact('showTime', 'id', 'event'));
     }
 
-    public function edit(Request $request,$id){  
-        dd($request);  
-        $id = decrypt($id);
-       $showTime = Showtime::where('id_event',$id)->get();
-        return view('ticket.index',compact('showTime'));
+    public function store(Request $request, $id)
+    {
+        // dd($request);
+        // $id = decrypt($id);
+
+        $request->validate([
+            "showtime_start" => "required",
+            "showtime_finish" => "required"
+        ]);
+
+        DB::beginTransaction();
+        try {
+
+            $query = Showtime::create([
+                'id_event' => $id,
+                'showtime_start' => $request->showtime_start,
+                'showtime_finish' => $request->showtime_finish
+            ]);
+
+
+            DB::commit();
+            // all good
+
+            return redirect('/show-time/' . $id)->with('status', 'Success Create Show Time');
+        } catch (\Exception $e) {
+            dd($e);
+            DB::rollback();
+            // something went wrong
+
+            return redirect('/show-time/' . $id)->with('failed', 'Failed Create Show Time');
+        }
     }
 
-    public function destroy(Request $request,$id){  
-        dd($request);  
-        $id = decrypt($id);
-       $showTime = Showtime::where('id_event',$id)->get();
-        return view('ticket.index',compact('showTime'));
+    public function edit(Request $request, $id)
+    {
+        // dd($request);
+        // $id = decrypt($id);
+        $showTime = Showtime::where('id_event', $id)->first();
+        dd($id, $showTime);
+
+        DB::beginTransaction();
+        try {
+            if ($showTime->isDirty()) {
+                //dd('berubah');
+                $query =  Showtime::where('id_event', $id)
+                    ->update([
+                        'showtime_start' => $request->showtime_start,
+                        'showtime_finish' => $request->showtime_finish
+                    ]);
+            } else {
+                $query =  Showtime::where('id_event', $id)
+                    ->update([
+                        'showtime_start' => $request->showtime_start,
+                        'showtime_finish' => $request->showtime_finish
+                    ]);
+                //dd('tidak berubah');
+            }
+            DB::commit();
+            // all good
+
+            return redirect('/show-time/' . $id)->with('status', 'Success Update Show Time');
+        } catch (\Exception $e) {
+            DB::rollback();
+            // something went wrong
+            return redirect('/show-time/' . $id)->with('failed', 'Failed Update Show Time');
+        }
+    }
+
+    public function destroy(Request $request, $id)
+    {
+        // dd($request);
+        // $id = decrypt($id);
+
+        DB::beginTransaction();
+        try {
+
+            $deleteShowTime = Showtime::where('id', $id)->delete();
+
+            DB::commit();
+            // all good
+
+            return redirect('/show-time/' . $id)->with('status', 'Success Delete Show Time');
+        } catch (\Exception $e) {
+            dd($e);
+            DB::rollback();
+            // something went wrong
+
+            return redirect('/show-time/' . $id)->with('failed', 'Failed Delete Show Time');
+        }
     }
 }
--- app/Http/Controllers/TicketPaymentController.php
+++ app/Http/Controllers/TicketPaymentController.php
@@ -2,10 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use App\Models\TicketPayment;
 use App\Models\Event;
 use App\Models\Dropdown;
+use Illuminate\Http\Request;
+use App\Models\TicketPayment;
+use Illuminate\Support\Facades\DB;
 
 class TicketPaymentController extends Controller
 {
@@ -18,24 +19,102 @@ class TicketPaymentController extends Controller
         $dropdownBankTransfer = Dropdown::where('category','Bank Transfer')->get();
         return view('ticket.indexPayment',compact('ticketPayment','id','event','dropdownPaymentMethod','dropdownBankTransfer'));
     }
-    public function store(Request $request, $id){    
-        dd($request);
-        $id = decrypt($id);
-       $ticketPayment = TicketPayment::where('id_event',$id)->get();
-        return view('ticket.index',compact('ticketPayment'));
+    public function store(Request $request, $id)
+    {
+        // dd($request);
+        // $id = decrypt($id);
+        $created_by = auth()->user()->email;
+        $request->validate([
+            "payment_method" => "required",
+            "bank_name" => "required",
+            "account_number" => "required",
+            "account_name" => "required",
+        ]);
+
+        DB::beginTransaction();
+        try {
+
+            $query = TicketPayment::create([
+                'id_event' => $id,
+                'payment_method' => $request->payment_method,
+                'bank_name' => $request->bank_name,
+                'account_number' => $request->account_number,
+                'account_name' => $request->account_name,
+                'created_by' => $created_by,
+            ]);
+
+
+            DB::commit();
+            // all good
+
+            return redirect('/ticket-payment/' . $id)->with('status', 'Success Create Ticket Payment');
+        } catch (\Exception $e) {
+            dd($e);
+            DB::rollback();
+            // something went wrong
+
+            return redirect('/ticket-payment/' . $id)->with('failed', 'Failed Create Ticket Payment');
+        }
     }
 
-    public function edit(Request $request,$id){  
-        dd($request);  
-        $id = decrypt($id);
-       $ticketPayment = TicketPayment::where('id_event',$id)->get();
-        return view('ticket.index',compact('ticketPayment'));
+    public function edit(Request $request, $id)
+    {
+        // dd($request);  
+        // $id = decrypt($id);
+        $ticketPayment = TicketPayment::where('id_event', $id)->first();
+        dd($id, $ticketPayment);
+
+        DB::beginTransaction();
+        try {
+            if ($ticketPayment->isDirty()) {
+                //dd('berubah');
+                $query =  TicketPayment::where('id_event', $id)
+                    ->update([
+                        'payment_method' => $request->payment_method,
+                        'bank_name,' => $request->bank_name,
+                        'account_number,' => $request->account_number,
+                        'account_name,' => $request->account_name,
+                    ]);
+            } else {
+                $query =  TicketPayment::where('id_event', $id)
+                    ->update([
+                        'payment_method' => $request->payment_method,
+                        'bank_name,' => $request->bank_name,
+                        'account_number,' => $request->account_number,
+                        'account_name,' => $request->account_name,
+                    ]);
+                //dd('tidak berubah');
+            }
+            DB::commit();
+            // all good
+
+            return redirect('/ticket-payment/' . $id)->with('status', 'Success Update Ticket Payment');
+        } catch (\Exception $e) {
+            DB::rollback();
+            // something went wrong
+            return redirect('/ticket-payment/' . $id)->with('failed', 'Failed Update Ticket Payment');
+        }
     }
 
     public function destroy(Request $request,$id){  
         dd($request);  
         $id = decrypt($id);
-       $ticketPayment = TicketPayment::where('id_event',$id)->get();
-        return view('ticket.index',compact('ticketPayment'));
+
+        DB::beginTransaction();
+        try {
+
+            $deleteTicketPayment = TicketPayment::where('id', $id)->delete();
+
+            DB::commit();
+            // all good
+
+            return redirect('/show-time/' . $id)->with('status', 'Success Delete Ticket Payment');
+        } catch (\Exception $e) {
+            dd($e);
+            DB::rollback();
+            // something went wrong
+
+            return redirect('/show-time/' . $id)->with('failed', 'Failed Delete Ticket Payment');
+        }
     }
 }
