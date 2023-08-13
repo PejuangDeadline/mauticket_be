65527b979d0ef28ca1a3f876afd2e43cf128fc68
Hardy Prabowo <hardy.prabowo@napinfo.co.id>
2023-08-13 20:49:32 +0700

    done API show event

--- app/Http/Controllers/API/ApiEventController.php
+++ app/Http/Controllers/API/ApiEventController.php
@@ -6,11 +6,13 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use App\Http\Controllers\API\ApiBaseController;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
 
 class ApiEventController extends ApiBaseController
 {
     public function eventSearch(Request $request){
-        // dd('hai');
+        // dd($request->all());
         $validator = Validator::make($request->all(), [
             'until' => 'required_with:from',
             'from' => 'required_with:until',
@@ -19,5 +21,32 @@ class ApiEventController extends ApiBaseController
         if($validator->fails()){
             return $this->sendError('Validation Error.', $validator->errors());       
         }
+
+        if ($request->from == '' || $request->until == '') {
+            $now = Carbon::now()->startOfMonth();
+            $date_now = Carbon::now()->lastOfMonth();
+        } else {
+            $now = $request->from;
+            $date_now = $request->until;
+        }
+
+        $query = DB::table('events')
+        ->leftJoinSub(function ($query) {
+            $query->select('*')
+                ->from('showtimes')
+                ->orderBy('id', 'asc')
+                ->limit(1);
+        }, 'showtimes', function ($join) {
+            $join->on('events.id', '=', 'showtimes.id_event');
+        })
+        ->select('events.*', 'showtimes.showtime_start')
+        ->whereNotNull('showtimes.showtime_start')
+        ->whereBetween(DB::raw("(STR_TO_DATE(showtime_start,'%Y-%m-%d'))"), [$now, $date_now])
+        ->where('events.event_name', 'LIKE', '%'.$request->event_name.'%')
+        ->where('events.city', 'LIKE', '%'.$request->city.'%')
+        ->get();
+        // dd($query);
+
+        return $this->sendResponse($query, 'Success Inquiry Event.');
     }
 }
