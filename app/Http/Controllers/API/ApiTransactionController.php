b079975998b04ea487bf91c8e9d58c0cf7c3411a
julia eka <eka47577@gmail.com>
2023-08-17 11:53:30 +0700

    api delete transactionTemp

--- app/Http/Controllers/API/ApiTransactionController.php
+++ app/Http/Controllers/API/ApiTransactionController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use App\Http\Controllers\API\ApiBaseController;
 use App\Models\RefCode;
+use App\Models\TransactionTemp;
 
 class ApiTransactionController extends ApiBaseController
 {
@@ -15,7 +16,15 @@ class ApiTransactionController extends ApiBaseController
     }
 
     public function chartDelete($id_chart){
-        
+        //delete Transaction Temp
+        $delTemp = TransactionTemp::where('id', $id_chart)->delete();
+
+        // dd($delTemp);
+        if ($delTemp) {
+            return $this->sendResponse($delTemp, 'Success Delete Transaction Temp');
+        } else {
+            return $this->sendError('Validation Error.', 'Transaction Temp is Invalid');
+        }
     }
 
     public function refCodeCheck(Request $request){
