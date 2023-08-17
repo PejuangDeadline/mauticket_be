1a8cff608aeee024c1591fb1dc78900a35fa3f2e
mhdtaufikrm <mtaufikramadhan27@gmail.com>
2023-08-17 10:34:02 +0700

    still on going

--- app/Http/Controllers/API/ApiTransactionController.php
+++ app/Http/Controllers/API/ApiTransactionController.php
@@ -6,55 +6,62 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Models\TransactionTemp;
+use App\Models\Transaction;
+use App\Models\TransactionDetail;
+use App\Http\Controllers\API\ApiBaseController;
+use Illuminate\Support\Facades\Validator;
 
 class ApiTransactionController extends Controller
 {
     public function chartAdd(Request $request)
-{
-    // Validate request data
-    $validatedData = $request->validate([
-        'id_user' => 'required|string|max:255',
-        'id_ticket_category' => 'required|string|max:255',
-        'id_event' => 'required|string|max:255',
-        'id_showtime' => 'required|string|max:255',
-        'price' => 'required|numeric',
-        'qty' => 'required|integer|min:1',
-    ]);
+    {
+        // Validate request data using the validate method
+        $validator = Validator::make($request->all(), [
+            'id_user' => 'required|string|max:255',
+            'id_ticket_category' => 'required|string|max:255',
+            'id_event' => 'required|string|max:255',
+            'id_showtime' => 'required|string|max:255',
+            'price' => 'required|numeric',
+            'qty' => 'required|integer|min:1',
+        ]);
 
-    try {
-        // Loop through the qty value and insert records
-        for ($i = 0; $i < $validatedData['qty']; $i++) {
-            // Start a database transaction for each iteration
-            DB::beginTransaction();
-
-            // Create a new transaction record
-            $transaction = TransactionTemp::create([
-                'id_user' => $validatedData['id_user'],
-                'id_ticket_category' => $validatedData['id_ticket_category'],
-                'id_event' => $validatedData['id_event'],
-                'id_showtime' => $validatedData['id_showtime'],
-                'price' => $validatedData['price'],
-            ]);
-
-            // Commit the transaction
-            DB::commit();
+        // Check if validation fails
+        if ($validator->fails()) {
+            return response()->json(['message' => 'Validation Error', 'errors' => $validator->errors()], 422);
         }
 
-        // Include input data and processed quantity in the success response
-        $responseData = [
-            'message' => 'Transactions added successfully',
-            'input_data' => $validatedData,
-            'processed_qty' => $validatedData['qty'],
-        ];
+        try {
+            // Loop through the qty value and insert records
+            for ($i = 0; $i < $request->qty; $i++) { // Use $request->qty instead of $validatedData['qty']
+                // Start a database transaction for each iteration
+                DB::beginTransaction();
 
-        return response()->json($responseData, 201);
-    } catch (\Exception $e) {
-        // Rollback the transaction if an exception occurs
-        DB::rollback();
+                // Create a new transaction record
+                $transaction = TransactionTemp::create([
+                    'id_user' => $request->id_user,
+                    'id_ticket_category' => $request->id_ticket_category,
+                    'id_event' => $request->id_event,
+                    'id_showtime' => $request->id_showtime,
+                    'price' => $request->price,
+                ]);
 
-        return response()->json(['message' => 'An error occurred', 'error' => $e->getMessage()], 500);
+                // Commit the transaction
+                DB::commit();
+            }
+
+            // Include input data and processed quantity in the success response
+            $response = [
+                'input_data' => $request->all(), // Use $request->all() to get all validated data
+                'processed_qty' => $request->qty,
+            ];
+            return $this->sendResponse($response,'Transactions added successfully');
+        } catch (\Exception $e) {
+            // Rollback the transaction if an exception occurs
+            DB::rollback();
+
+            return response()->json(['message' => 'An error occurred', 'error' => $e->getMessage()], 500);
+        }
     }
-}
 
     public function chartDelete($id_chart){
         
@@ -68,8 +75,54 @@ class ApiTransactionController extends Controller
         
     }
 
-    public function checkoutStore(Request $request){
-        
+    public function checkoutStore(Request $request)
+    {
+        // Validate request data
+        $validatedData = $request->validate([
+            'id_user' => 'required|integer',
+            'no_transaction' => 'required|string|max:255',
+            'items' => 'required|array|min:1',
+            // ... add more validation rules for other fields
+        ]);
+
+        try {
+            // Start a database transaction
+            DB::beginTransaction();
+
+            // Create the main transaction record
+            $transaction = Transaction::create([
+                'id_user' => $validatedData['id_user'],
+                'no_transaction' => $validatedData['no_transaction'],
+                // ... add values for other fields
+            ]);
+
+            // Process the items and create transaction detail records
+            foreach ($validatedData['items'] as $item) {
+                TransactionDetail::create([
+                    'transaction_header_id' => $transaction->id,
+                    'id_ticket_category' => $item['id_ticket_category'],
+                    'id_event' => $item['id_event'],
+                    'id_showtime' => $item['id_showtime'],
+                    'price' => $item['price'],
+                    // ... add values for other fields
+                ]);
+            }
+
+            // Commit the transaction
+            DB::commit();
+
+            // Include a success message in the response
+            $responseData = [
+                'message' => 'Checkout completed successfully',
+            ];
+
+            return response()->json($responseData, 201);
+        } catch (\Exception $e) {
+            // Rollback the transaction if an exception occurs
+            DB::rollback();
+
+            return response()->json(['message' => 'An error occurred', 'error' => $e->getMessage()], 500);
+        }
     }
 
     public function paymentUpload(Request $request){
